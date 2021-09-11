db4b240cb367f61d96ace3568d132c4bd67dda44
ImDany <dangzpz@gmail.com>
2021-09-11 20:18:39 +0100

    New Post

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210810.1632';
+const cacheName = 'chirpy-20210911.2013';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
